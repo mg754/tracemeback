f154610bd17db92105b266c8a6a684b9c8d931c9
michael <mgergely@sfu.ca>
2018-07-01 21:33:42 -0700

    demo

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,12 @@ ActiveRecord::Schema.define(version: 20140707111715) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "widgets", force: :cascade do |t|
+  create_table "widgets", id: :serial, force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "stock"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
